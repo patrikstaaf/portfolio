8deeb9d46ed3e1f1f40bb8ee226d4c07087e3da9
patrikstaaf <staaf.patrik@gmail.com>
2022-05-04 19:13:10 +0200

    Add repo link to portfolio in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,18 @@
 const Footer = () => {
   return (
     <footer className='mt-10 px-4'>
-      <small>{new Date().getFullYear()} &copy; Patrik Staaf.</small>
+      <small>{new Date().getFullYear()} &copy; Patrik Staaf</small>
+      <span className='px-1 text-gray-500'>•</span>
+      <small className='text-gray-500'>
+        <a
+          href='https://github.com/patrikstaaf/portfolio'
+          className='hover:underline'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Portfolio repo
+        </a>
+      </small>
     </footer>
   );
 };
